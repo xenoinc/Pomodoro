122219734aad627886429a25171156a42f11601f
Damian Suess <djsuess@gmail.com>
2019-02-28 10:42:42 +0700

    updated version to 1.3.53 (forgot last time)

--- source/Desktop/Properties/AssemblyInfo.cs
+++ source/Desktop/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A simple desktop pomodoro timer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Xeno Innovations, Inc.")]
-[assembly: AssemblyProduct("Pomodoro")]
+[assembly: AssemblyProduct("Pomodoro 🍅")]
 [assembly: AssemblyCopyright("Copyright © Xeno Innovations 2016-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.52")]
-[assembly: AssemblyFileVersion("1.2.52")]
+[assembly: AssemblyVersion("1.3.53")]
+[assembly: AssemblyFileVersion("1.3.53")]
